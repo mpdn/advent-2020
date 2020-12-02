9abba2700e713441cb9275c9b2a6d0af25289e4c
Mike Pedersen <mike@mikepedersen.dk>
2020-12-02 17:13:19 +0100

    2

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-const ONE: &str = std::include_str!("1.txt");
+use scan_fmt::scan_fmt;
 
 fn one() {
-    let expenses: Vec<u32> = ONE.lines()
+    let expenses: Vec<u32> = std::include_str!("1.txt")
+        .lines()
         .map(|s| s.parse().unwrap())
         .collect();
 
-    'outer_a:
-    for a in &expenses {
+    'outer_a: for a in &expenses {
         for b in &expenses {
             if a + b == 2020 {
                 println!("{}", a * b);
@@ -15,8 +15,7 @@ fn one() {
         }
     }
 
-    'outer_b:
-    for a in &expenses {
+    'outer_b: for a in &expenses {
         for b in &expenses {
             for c in &expenses {
                 if a + b + c == 2020 {
@@ -28,6 +27,36 @@ fn one() {
     }
 }
 
+fn two() {
+    let passwords: Vec<_> = std::include_str!("2.txt")
+        .lines()
+        .map(|s| scan_fmt!(s, "{d}-{d} {[a-z]}: {}", usize, usize, char, String).unwrap())
+        .collect();
+
+    let n_valid_a = passwords
+        .iter()
+        .filter(|&&(min, max, ch, ref pass)| {
+            (min..=max).contains(&pass.chars().filter(|&c| c == ch).count())
+        })
+        .count();
+
+    println!("{}", n_valid_a);
+
+    let n_valid_b = passwords
+        .iter()
+        .filter(|&&(a, b, ch, ref pass)| {
+            pass.chars()
+                .enumerate()
+                .filter(|&(i, c)| (i == a - 1 || i == b - 1) && c == ch)
+                .count()
+                == 1
+        })
+        .count();
+
+    println!("{}", n_valid_b);
+}
+
 fn main() {
-    one();  
+    one();
+    two();
 }
